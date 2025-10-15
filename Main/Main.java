bd95b080436dd8e5a3401e6133709cade7827eb7
duongleminh03qb <duongleminh03qb@gmail.com>
2025-10-15 09:09:48 +0700

    println

--- Main/Main.java
+++ Main/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.print("Hello World! ");
+        System.out.println("I will print on the same line.");
+        System.out.println(3 + 3);
+        System.out.println(2 * 5);
     }
 }
